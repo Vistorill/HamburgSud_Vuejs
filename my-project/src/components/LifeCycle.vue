2fb2d55118cd4b065add63599ab7b7bc9822d6c8
Ma4rcos <40584107+Vistorill@users.noreply.github.com>
2022-10-26 20:29:10 -0300

    yesterday

---
+++ my-project/src/components/LifeCycle.vue
@@ -0,0 +1,28 @@
+<template>
+    <h1>Meu Nome é: {{nome}}</h1>
+</template>
+
+<script>
+    export default{
+       name:'LifeCycle',
+       data(){
+       return{
+        nome:'Ainda nao sei'
+
+       }
+       },
+       created(){
+        setTimeout(()=>{
+            this.nome = 
+        'Matheus'
+        },1000)
+
+       },
+          mounted(){
+         setTimeout(()=>{
+             this.nome = "Pedro"
+         },2000)
+            
+          }
+          }
+</script>
---
+++ my-project/src/components/PrimeiroComponente.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Ola Vue!!</h1>
+        <p>Ola Meu nome é {{nome}} e sou {{profissao}}</p>
+    </div>
+   
+</template>
+
+<script>
+export default{
+    name:'PrimeiroComponente',
+    data(){
+        return{
+            nome:'Marcos',
+            profissao:'Desenvolvedor de softwares'
+        }
+    }
+    
+    
+}
+</script>
